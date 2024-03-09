eec0ec294785333f9bd35b509e6b3db3353c82b2
Emily <145737671+batesemily@users.noreply.github.com>
2024-03-08 16:13:59 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,9 +34,8 @@ if __name__ == "__main__":
         name="satsq",
         # version="1",
         packages=['configs', 'dataset', 'nets', 'solver', 'utils'],
-        package_dir={'configs': 'core/configs',
-                    'dataset': 'core/dataset',
-                    'nets': 'core/nets',
-                    'solver': 'core/solver',
-                    'utils': 'core/utils'}
+        package_dir={'configs': 'core/',
+                    'dataset': 'core/',
+                    'solver': 'core/',
+                    'utils': 'core/'}
     )
